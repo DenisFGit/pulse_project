a75172c5c9374ba0e6038370169ed6ba7acc87a4
Denis <denis>
2024-10-31 22:44:37 +0200

    made modal tabs and validation

--- src/js/script.js
+++ src/js/script.js
@@ -145,17 +145,6 @@ document.querySelectorAll('.catalog-item__link').forEach(function (element, i) {
     });
 });
 
-// (function ($) {
-//     $(function () {
-
-//         $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-//             $(this)
-//                 .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-//                 .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-//         });
-
-//     });
-// });
 
 $(function () {
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
@@ -164,4 +153,74 @@ $(function () {
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index())
             .addClass('catalog__content_active');
     });
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn();
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+    // $('.button_tab').on('click', function () {
+    //     $('.overlay, #order').fadeIn();
+    // })
+
+    $('.button_tab').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__desc').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        });
+    });
+
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: "required",
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: "Пожалуйста введите свое имя",
+    //         phone: "Пожалуйста введите свой номер телефона",
+    //         email: {
+    //             required: "Пожалуйста введите свою почту",
+    //             email: "Ваша почта должна быть в формате name@domain.com"
+    //         }
+    //     }
+    // });
+    // $('#order form').validate();
+
+
+    function validateForm(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста введите свое имя",
+                phone: "Пожалуйста введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста введите свою почту",
+                    email: "Ваша почта должна быть в формате name@domain.com"
+                }
+            }
+        });
+    }
+
+    validateForm('#consultation-form');
+    validateForm('#consultation form');
+    validateForm('#order form');
+
+    $('input[name=phone]').mask("+38(999) 999-99-99");
 });
+
+
